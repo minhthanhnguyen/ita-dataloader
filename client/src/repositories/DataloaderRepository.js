e3257fcdc24e1b212ec62eeb787eeb797bd57a00
lipsotiko <lipsotiko@gmail.com>
2019-10-01 17:00:32 -0400

    Updated deployment scripts; vangos azure subscription

--- client/src/repositories/DataloaderRepository.js
+++ client/src/repositories/DataloaderRepository.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 export default class DataloaderRepository {
-  async _save(containerName, file) {
+  async _save (containerName, file) {
     let fileSaveResponse = await axios({
       url: '/api/save/file',
       method: 'PUT',
@@ -13,12 +13,12 @@ export default class DataloaderRepository {
     return fileSaveResponse.data
   }
 
-  async _getBusinessUnits() {
+  async _getBusinessUnits () {
     let businessUnitResponse = await axios.get('/api/business-units')
     return businessUnitResponse.data
   }
 
-  async _getDataloaderConfig(containerName) {
+  async _getDataloaderConfig (containerName) {
     let dataSetConfigsResponse = await axios.get('/api/configuration', {
       params: {
         containerName
@@ -28,7 +28,7 @@ export default class DataloaderRepository {
     return dataSetConfigsResponse.data
   }
 
-  async _saveDataloaderConfig(dataloaderConfig, containerName) {
+  async _saveDataloaderConfig (dataloaderConfig, containerName) {
     let configSaveResponse = await axios({
       url: '/api/save/configuration',
       method: 'PUT',
@@ -43,7 +43,7 @@ export default class DataloaderRepository {
     return configSaveResponse
   }
 
-  async _getStorageContent(containerName) {
+  async _getStorageContent (containerName) {
     let storageContentResponse = await axios.get('/api/storage-content', {
       params: {
         containerName
@@ -53,7 +53,7 @@ export default class DataloaderRepository {
     return storageContentResponse.data
   }
 
-  async _startIngestProcess(containerName) {
+  async _startIngestProcess (containerName) {
     let ingestProcessResponse = await axios.get('/api/ingest', {
       params: {
         containerName
@@ -63,7 +63,7 @@ export default class DataloaderRepository {
     return ingestProcessResponse
   }
 
-  async _getIngestStatus(containerName) {
+  async _getIngestStatus (containerName) {
     let ingestStatusResponse = await axios.get('/api/ingest/status', {
       params: {
         containerName
