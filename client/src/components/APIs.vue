5473970dd2aad3f1920eded46578ff2dec7d65e6
lipsotiko <lipsotiko@gmail.com>
2019-10-28 15:27:00 -0400

    Added some basic documentation for the public APIs

--- client/src/components/APIs.vue
+++ client/src/components/APIs.vue
@@ -4,7 +4,21 @@
     <div class="content">
       <dataloader-menu v-bind:containerName="containerName" />
       <div class="sub-content">
-        {{businessUnitName}} - APIs
+        <section>
+          <h1 class="md-title">Storage Containers</h1>
+          <a href="/api/storage-containers" target="_blank">/api/storage-containers</a>
+          <p>Returns an array of $containerName</p>
+        </section>
+        <section>
+          <h1 class="md-title">Container Metadata</h1>
+          <p>/api/container-metadata?containerName=$containerName</p>
+          <p>Returns the metadata associated with a given storage container: $containerName, $fileName</p>
+        </section>
+        <section>
+          <h1 class="md-title">FTA Tariff Rates</h1>
+          <p>/api/extract/fta/tariff-rates?containerName=$containerName&amp;fileName=$fileName</p>
+          <p>Returns tariff rates for the given file with Rule of Origin applied</p>
+        </section>
         <div v-if="loading" class="loading">
           <md-progress-bar md-mode="indeterminate"></md-progress-bar>
         </div>
