d2fcd0967dc4c21d49bfdce01c6a6ded29febd74
lipsotiko <lipsotiko@gmail.com>
2019-11-01 08:58:40 -0400

    Added table for IESE VCPE data in Select USA data factory pipeline

--- src/main/resources/db/migration/V1.0__Initial_Schema.sql
+++ src/main/resources/db/migration/V1.0__Initial_Schema.sql
@@ -53,7 +53,8 @@ create TABLE IMF_WEODATA (
     [2023] VARCHAR(25),
     [2024] VARCHAR(25),
     [Estimates Start After] VARCHAR(255)
-)
+);
+go
 
 create TABLE WORLDBANK_EASE_COUNTRY_METADATA (
     COUNTRY_CODE VARCHAR(50),
@@ -61,7 +62,8 @@ create TABLE WORLDBANK_EASE_COUNTRY_METADATA (
     INCOME_GROUP VARCHAR(255),
     SPECIAL_NOTES VARCHAR(1500),
     TABLE_NAME VARCHAR(100)
-)
+);
+go
 
 create TABLE WORLDBANK_EASE_COUNTRY_INDEX (
     [Country Name] VARCHAR(255),
@@ -127,7 +129,8 @@ create TABLE WORLDBANK_EASE_COUNTRY_INDEX (
     [2016] VARCHAR(25),
     [2017] VARCHAR(25),
     [2018] VARCHAR(25)
-)
+);
+go
 
 create TABLE BEA_NIPA (
     [TableName] VARCHAR(255),
@@ -140,7 +143,8 @@ create TABLE BEA_NIPA (
     [UNIT_MULT] INT,
     [DataValue] INT,
     [NoteRef] INT
-)
+);
+go
 
 create TABLE BEA_MNE_COUNTRY (
     [DIRECTIONOFINVESTMENT] VARCHAR(255),
@@ -155,7 +159,8 @@ create TABLE BEA_MNE_COUNTRY (
     [TableScale] VARCHAR(255),
     [DataValueUnformatted] VARCHAR(255),
     [DataValue] VARCHAR(255)
-)
+);
+go
 
 create TABLE BEA_MNE_COUNTRY_BY_INDUSTRY (
     [DIRECTIONOFINVESTMENT] VARCHAR(255),
@@ -170,7 +175,8 @@ create TABLE BEA_MNE_COUNTRY_BY_INDUSTRY (
     [TableScale] VARCHAR(255),
     [DataValueUnformatted] VARCHAR(255),
     [DataValue] VARCHAR(255)
-)
+);
+go
 
 create TABLE BEA_MNE_COUNTRY_UBO_BY_INDUSTRY (
     [DIRECTIONOFINVESTMENT] VARCHAR(255),
@@ -185,7 +191,8 @@ create TABLE BEA_MNE_COUNTRY_UBO_BY_INDUSTRY (
     [TableScale] VARCHAR(255),
     [DataValueUnformatted] VARCHAR(255),
     [DataValue] VARCHAR(255)
-)
+);
+go
 
 create TABLE BEA_MNE_INDUSTRY (
     [DIRECTIONOFINVESTMENT] VARCHAR(255),
@@ -200,7 +207,8 @@ create TABLE BEA_MNE_INDUSTRY (
     [TableScale] VARCHAR(255),
     [DataValueUnformatted] VARCHAR(255),
     [DataValue] VARCHAR(255)
-)
+);
+go
 
 create TABLE BEA_ITA (
     [Indicator] VARCHAR(255),
@@ -213,7 +221,8 @@ create TABLE BEA_ITA (
     [CL_UNIT] VARCHAR(255),
     [UNIT_MULT] VARCHAR(255),
     [DataValue] INT
-)
+);
+go
 
 create TABLE OTEXA_DATA_SET_CAT (
    CTRY_ID INTEGER
@@ -221,27 +230,29 @@ create TABLE OTEXA_DATA_SET_CAT (
   , SYEF DECIMAL(5,2)
   , HEADER_ID VARCHAR(25)
   , VAL DECIMAL(30, 10)
-)
+);
 
 create TABLE OTEXA_CATEGORY_REF (
    CAT_ID  INTEGER
   , CAT_DESCRIPTION VARCHAR(255)
-)
+);
+go
 
 create TABLE OTEXA_COUNTRY_REF (
    CTRY_ID  INTEGER
   , CTRY_DESCRIPTION VARCHAR(255)
-)
+);
+go
 
 create TABLE OTEXA_HEADER_REF (
    HEADER_ID  VARCHAR(15)
   , YR INTEGER
   , HEADER_DESCRIPTION VARCHAR(255)
   , HEADER_TYPE VARCHAR(255)
-)
+);
 go
 
-create VIEW OTEXA_DATA_SET_CAT_VW
+create view OTEXA_DATA_SET_CAT_VW
 as
 select details.CTRY_ID
     , details.CAT_ID
@@ -250,4 +261,39 @@ select details.CTRY_ID
 from OTEXA_DATA_SET_CAT details
 inner join OTEXA_HEADER_REF hdr
 on details.HEADER_ID = hdr.HEADER_ID;
+go
+
+create TABLE IESE_VCPE_INDEX (
+   Regioncountry                                           VARCHAR(20) NOT NULL
+  , Score_or_Rank                                          VARCHAR(5) NOT NULL
+  , [0_Index]                                              NUMERIC(5,1) NOT NULL
+  , [1_Economic_Activity]                                  NUMERIC(5,1) NOT NULL
+  , [11_Size_of_the_Economy_GDP]                           NUMERIC(5,1) NOT NULL
+  , [12_Expected_Real_GDP_Growth]                          NUMERIC(5,1) NOT NULL
+  , [13_Unemployment]                                      NUMERIC(5,1) NOT NULL
+  , [2_Capital_Market]                                     NUMERIC(5,1) NOT NULL
+  , [21_Size_and_Liquidity_of_the_Stock_Market]            NUMERIC(5,1) NOT NULL
+  , [211_Market_Capitalization_of_Listed_Companies]        NUMERIC(5,1) NOT NULL
+  , [23_IPOs_and_Public_Issuing_Activity]                  NUMERIC(5,1) NOT NULL
+  , [24_MA_Market]                                         NUMERIC(5,1) NOT NULL
+  , [25_Debt_and_Credit_Market]                            NUMERIC(5,1) NOT NULL
+  , [26_Bank_Non_Performing_Loans_to_Toal_Gross_Loans]     NUMERIC(5,1) NOT NULL
+  , [27_Financial_Market_Sophistication]                   NUMERIC(5,1) NOT NULL
+  , [3_Taxation]                                           NUMERIC(5,1) NOT NULL
+  , [31_Entrepreneurial_Tax_Incentives_and_Admin_Burden]   NUMERIC(5,1) NOT NULL
+  , [4_Investor_Protection_and_Corporate_Governance]       NUMERIC(5,1) NOT NULL
+  , [41_Quality_of_Corporate_Governance]                   NUMERIC(5,1) NOT NULL
+  , [42_Security_of_Property_Rights]                       NUMERIC(5,1) NOT NULL
+  , [43_Quality_of_Legal_Enforcement]                      NUMERIC(5,1) NOT NULL
+  , [5_Human_and_Social_Environment]                       NUMERIC(5,1) NOT NULL
+  , [51_Education_and_Human_Capital]                       NUMERIC(5,1) NOT NULL
+  , [52_Labor_Regulations]                                 NUMERIC(5,1) NOT NULL
+  , [53_Bribing_and_Corruption]                            NUMERIC(5,1) NOT NULL
+  , [6_Entrepreneurial_Opportunities]                      NUMERIC(5,1) NOT NULL
+  , [61_Innovation]                                        NUMERIC(5,1) NOT NULL
+  , [62_Scientific_and_Technical_Journal_Articles]         NUMERIC(5,1) NOT NULL
+  , [63_Burdens_of_Starting_and_Running_a_Business]        NUMERIC(5,1) NOT NULL
+  , [64_Simplicity_of_Closing_a_Business]                  NUMERIC(5,1) NOT NULL
+  , [65_Corporate_RD]                                      NUMERIC(5,1) NOT NULL
+);
 go
