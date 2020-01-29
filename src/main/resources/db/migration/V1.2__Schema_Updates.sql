3f0fac0b2104cf9b7f1c0e25351e8af125ac1cfa
lipsotiko <lipsotiko@gmail.com>
2020-01-29 17:04:00 -0500

    Added OECD GDP schema for Select USA

---
+++ src/main/resources/db/migration/V1.2__Schema_Updates.sql
@@ -0,0 +1,18 @@
+CREATE TABLE [OECD_GDP] (
+   [LOCATION]              VARCHAR(255)
+  ,[Country]               VARCHAR(255)
+  ,[Subject]               VARCHAR(255)
+  ,[Measure]               VARCHAR(255)
+  ,[TIME]                  INTEGER
+  ,[Unit_Code]             VARCHAR(255)
+  ,[Unit]                  VARCHAR(255)
+  ,[PowerCode_Code]        INTEGER
+  ,[PowerCode]             VARCHAR(255)
+  ,[Reference_Period_Code] VARCHAR(255)
+  ,[Reference_Period]      VARCHAR(255)
+  ,[Value]                 DECIMAL(30,10)
+  ,[Flag_Codes]            VARCHAR(255)
+  ,[Flags]                 VARCHAR(255)
+);
+GO
+
