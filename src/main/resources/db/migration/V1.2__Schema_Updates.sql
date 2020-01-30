fcc8c545625479c8c9ffbbd064b54b150bd9d15b
lipsotiko <lipsotiko@gmail.com>
2020-01-30 10:52:51 -0500

    Added more OCED tables for Select USA

--- src/main/resources/db/migration/V1.2__Schema_Updates.sql
+++ src/main/resources/db/migration/V1.2__Schema_Updates.sql
@@ -16,3 +16,36 @@ CREATE TABLE [OECD_GDP] (
 );
 GO
 
+CREATE TABLE [OECD_STRICTNESS_OF_EMPLOYMENT_PROTECTION](
+  [Country]                VARCHAR(255)
+  ,[Series]                VARCHAR(255)
+  ,[TIME]                  INTEGER
+  ,[Unit_Code]             VARCHAR(255)
+  ,[Unit]                  VARCHAR(255)
+  ,[PowerCode_Code]        INTEGER
+  ,[PowerCode]             VARCHAR(255)
+  ,[Reference_Period_Code] VARCHAR(255)
+  ,[Reference_Period]      VARCHAR(255)
+  ,[Value]                 DECIMAL(30,10)
+  ,[Flag_Codes]            VARCHAR(255)
+  ,[Flags]                 VARCHAR(255)
+);
+GO
+
+CREATE TABLE [OECD_AVERAGE_ANNUAL_HOURS_WORKED_PER_WORKER](
+  [Country]                 VARCHAR(255)
+  ,[EMPSTAT]                VARCHAR(255)
+  ,[Employment_status]      VARCHAR(255)
+  ,[Frequency]              VARCHAR(255)
+  ,[TIME]                   INTEGER
+  ,[Unit_Code]              VARCHAR(255)
+  ,[Unit]                   VARCHAR(255)
+  ,[PowerCode_Code]         INTEGER
+  ,[PowerCode]              VARCHAR(255)
+  ,[Reference_Period_Code]  VARCHAR(255)
+  ,[Reference_Period]       VARCHAR(255)
+  ,[Value]                  DECIMAL(30,10)
+  ,[Flag_Codes]             VARCHAR(255)
+  ,[Flags]                  VARCHAR(255)
+);
+GO
