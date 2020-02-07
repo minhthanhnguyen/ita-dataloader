1a876f67c5a9953bf35642b47165b06c523e0eeb
lipsotiko <lipsotiko@gmail.com>
2020-02-07 09:23:30 -0500

    Added additional tables for select USA, and updated the ingest configuration

---
+++ src/main/resources/db/migration/V1.3__Schema_Updates.sql
@@ -0,0 +1,59 @@
+create TABLE BEA_MNE_COUNTRY_BY_TYPE (
+    [DIRECTIONOFINVESTMENT] VARCHAR(255),
+    [OwnershipLevel] INT,
+    [Year] INT,
+    [SeriesID] INT,
+    [SeriesName] VARCHAR(255),
+    [Row] VARCHAR(255),
+    [Column] VARCHAR(255),
+    [RowCode] VARCHAR(255),
+    [ColumnCode] VARCHAR(255),
+    [TableScale] VARCHAR(255),
+    [DataValueUnformatted] VARCHAR(255),
+    [DataValue] VARCHAR(255),
+    [ColumnParent] VARCHAR(255),
+    [ColumnParentCode] INT,
+    [ColumnGParent] VARCHAR(255),
+    [ColumnGParentCode] INT
+);
+go
+
+create TABLE BEA_MNE_STATE_BY_TYPE (
+    [DIRECTIONOFINVESTMENT] VARCHAR(255),
+    [OwnershipLevel] INT,
+    [Year] INT,
+    [SeriesID] INT,
+    [SeriesName] VARCHAR(255),
+    [Row] VARCHAR(255),
+    [Column] VARCHAR(255),
+    [RowCode] VARCHAR(255),
+    [ColumnCode] VARCHAR(255),
+    [TableScale] VARCHAR(255),
+    [DataValueUnformatted] VARCHAR(255),
+    [DataValue] VARCHAR(255),
+    [ColumnParent] VARCHAR(255),
+    [ColumnParentCode] INT,
+    [ColumnGParent] VARCHAR(255),
+    [ColumnGParentCode] INT
+);
+go
+
+create TABLE BEA_MNE_INDUSTRY_BY_TYPE (
+    [DIRECTIONOFINVESTMENT] VARCHAR(255),
+    [OwnershipLevel] INT,
+    [Year] INT,
+    [SeriesID] INT,
+    [SeriesName] VARCHAR(255),
+    [Row] VARCHAR(255),
+    [Column] VARCHAR(255),
+    [RowCode] VARCHAR(255),
+    [ColumnCode] VARCHAR(255),
+    [TableScale] VARCHAR(255),
+    [DataValueUnformatted] VARCHAR(255),
+    [DataValue] VARCHAR(255),
+    [ColumnParent] VARCHAR(255),
+    [ColumnParentCode] INT,
+    [ColumnGParent] VARCHAR(255),
+    [ColumnGParentCode] INT
+);
+go
